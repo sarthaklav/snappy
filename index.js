4defd309c8ac65804af7a2125a5e474a317fd2ce
Sarthak Lav <sarthakchaudhary52@gmail.com>
2021-03-27 23:52:51 +0530

    added : basic working alert
    alert working with setTimeout

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hi I am form index.js... so it's linked...s");
+// console.log("Hi I am form index.js... so it's linked...s");
+
+// const btn = document.querySelector('#alert-btn');
+// if( btn){
+//     btn.addEventListener('click', triggerAlert);
+// }
+
+// function triggerAlert() {
+//     console.log("triggerAlert was called!!");
+
+//   var alertEl = document.querySelector('.alert');
+  
+//   alertEl.setAttribute("role", "alert");
+// }
+
+
+// function showAlert(){
+//     console.log("show alert was called...")
+//     document.getElementById("expirationWarning").className = ""; 
+// }
+
+const showAlertTimeout = document.getElementById('show-alert-timeout');
+
+if(showAlertTimeout){
+    showAlertTimeout.addEventListener('click', triggerAlertMessage);
+}
+
+function triggerAlertMessage(){
+    console.log("trigger alert message was called");
+    const alert = document.getElementById('alert-message-timeout').style.visibility="visible";
+
+    setTimeout(function(){
+         document.getElementById('alert-message-timeout').style.visibility="hidden";
+        // document.getElementById('alert-message').className="visible";
+    },2000);
+}
