d28ef3cb816715e5c40b6a3f5bc86e068631c16c
Sarthak Lav <sarthakchaudhary52@gmail.com>
2021-03-28 16:26:19 +0530

    added : 2 type of alerts are done

--- index.js
+++ index.js
@@ -22,15 +22,37 @@
 const showAlertTimeout = document.getElementById('show-alert-timeout');
 
 if(showAlertTimeout){
-    showAlertTimeout.addEventListener('click', triggerAlertMessage);
+    showAlertTimeout.addEventListener('click', triggerTimeoutAlert);
 }
 
-function triggerAlertMessage(){
+function triggerTimeoutAlert(){
     console.log("trigger alert message was called");
-    const alert = document.getElementById('alert-message-timeout').style.visibility="visible";
+     document.getElementById('alert-message-timeout').style.visibility="visible";
 
     setTimeout(function(){
          document.getElementById('alert-message-timeout').style.visibility="hidden";
-        // document.getElementById('alert-message').className="visible";
-    },2000);
+    },3000);
+}
+
+
+const showAlertCancel = document.getElementById('show-alert-cancel');
+if(showAlertCancel) {
+    showAlertCancel.addEventListener('click',triggerCancelAlert);
+}
+function triggerCancelAlert(){
+    console.log("trigger cancel alert was called");
+    document.getElementById('alert-message-cancel').style.visibility="visible";
+}
+
+
+const closeAlertCancel = document.getElementById('close-alert-cancel');
+
+if(closeAlertCancel){
+    console.log("if element is working");
+    closeAlertCancel.addEventListener('click', closeTimeoutAlertMessage);
+}
+
+function closeTimeoutAlertMessage(){
+    console.log("close event handller was called");
+    document.getElementById('alert-message-cancel').style.visibility="hidden";
 }
